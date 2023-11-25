38c5eaa2230c798952a255f6802953268a7e292b
grupo3c13 <grupo3c13@gmail.com>
2023-11-24 23:20:03 -0300

    agrege el CorsConfig

---
+++ src/main/java/com/example/demo/entity/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+public class CorsConfig {   @Bean
+CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5173","http://localhost:5173"));
+    configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","Origin", "Content-Type", "Accept"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+}
+}
