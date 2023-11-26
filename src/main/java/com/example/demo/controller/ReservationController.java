37eb010ff99f6133c93881b8fa42412e0dd74331
DayanaSilvaSolcre <dayana.silva@solcre.com>
2023-11-25 22:41:22 -0300

    Reservation clases (falta la logica)

---
+++ src/main/java/com/example/demo/controller/ReservationController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    //hacer una reserva
+    //listar todas las reservas
+    //buscar reserva por producto (id)
+    //eliminar reserva
+    //editar reserva
+}
